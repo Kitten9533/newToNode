8cc5be976e15fb22b69560b1ab13e329b32358f9
Kitten9533 <1173860556@qq.com>
2017-03-07 00:39:06 +0800

    add
    curring

--- jiade.js
+++ jiade.js
@@ -68,3 +68,23 @@ var my = mi(function(){
 });
 
 console.log(my(1,2,3)(10))
+
+
+var add = function(fn){
+	var args = [].slice.call(arguments);
+	var sum = 0;
+	for(var key in args){
+		sum += args[key];
+	}
+	//sum
+	var tmp = function (y) {
+        sum = sum + y;
+        return tmp;
+    };
+    tmp.toString = function () {
+        return sum;
+    };
+    return tmp;
+};
+
+console.log(add(1,2,3)(10))
